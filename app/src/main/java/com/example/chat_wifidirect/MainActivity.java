a0b25e76f29c37508060a0368176aa9459b3adf6
lmach14 <lmach14@freeuni.edu.ge>
2019-07-18 19:24:29 +0400

    add tool bar

--- app/src/main/java/com/example/chat_wifidirect/MainActivity.java
+++ app/src/main/java/com/example/chat_wifidirect/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.chat_wifidirect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
