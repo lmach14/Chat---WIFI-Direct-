6db24947acdcb10cd53a1fddd4cda6b29480edc7
archil <akoba14@freeuni.edu.ge>
2019-07-21 22:47:23 +0400

    update massagepresenter

--- app/src/main/java/com/example/chat_wifidirect/Presenters/MessagePagePresenter.java
+++ app/src/main/java/com/example/chat_wifidirect/Presenters/MessagePagePresenter.java
@@ -2,6 +2,7 @@ package com.example.chat_wifidirect.Presenters;
 
 import com.example.chat_wifidirect.Contracts.MessageContract;
 import com.example.chat_wifidirect.Models.MessageModel;
+import com.example.chat_wifidirect.data.ChatJoinEntity;
 import com.example.chat_wifidirect.data.ChatRepository;
 import com.example.chat_wifidirect.data.MessageEntity;
 
@@ -12,16 +13,21 @@ public class MessagePagePresenter implements MessageContract.Presenter {
 
     private MessageContract.View view;
     ChatRepository chatRepository ;
+    ChatJoinEntity chat;
 
     public MessagePagePresenter(MessageContract.View view) {
         this.view = view;
         this.chatRepository = new ChatRepository();
 
+
     }
 
     @Override
     public void start(Long id) {
+        chat = chatRepository.getChatByid(id);
         view.initRecyclerView(getMessages(id));
+        view.insertHeader(chat.chat.getName(), chat.chat.getDate());
+
     }
 
 
@@ -33,7 +39,7 @@ public class MessagePagePresenter implements MessageContract.Presenter {
 
 
     public List getMessages(Long id) {
-        List<MessageEntity> messages = chatRepository.selectMessagesByChatId(id);
+        List<MessageEntity> messages = chat.mesageList;
         List<MessageModel> messages_model= new ArrayList<>();
         for (MessageEntity messageEntity: messages) {
             messages_model.add(new MessageModel(messageEntity.getId(), messageEntity.getChat_id(), messageEntity.isIs_sender(), messageEntity.getMessage(), messageEntity.getDate()));
